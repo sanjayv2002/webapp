10bc963611c5a483bb79707de5ecd574c0b6a4c7
sanjayv2002 <70307085+sanjayv2002@users.noreply.github.com>
2023-02-14 09:20:02 +0000

    both front and back end working in docker

--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const Test = () => {
 
     useEffect(() => {
-        fetch('http://127.0.0.1:5000/').then(
+        fetch('http://127.0.0.1:5000/',{headers:{'Content-Type':'application/json'}}).then(
             response => response.json()
         ).then( result => console.log(result))
     }, [])
