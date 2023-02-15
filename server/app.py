f26daeb17d6b09173ffa3019e0f72d5c62d81114
SANJAY KUMAR V <70307085+sanjayv2002@users.noreply.github.com>
2023-02-15 10:20:02 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask 
 from flask_cors import cross_origin 
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0')
+    app.run(debug = True, port = os.getenv("PORT", default = 5000))
